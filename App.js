182497e11d1f34204230a92b17374b2fc1178007
VinayVidhehi <vinayvidhehi@gmail.com>
2024-05-31 19:50:54 +0530

    safe area view added

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { enableScreens } from "react-native-screens";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Home from "./Components/Home";
 import Combo from "./Components/Combo";
 import Cart from "./Components/Cart";
@@ -12,7 +14,9 @@ import Order from "./Components/Order";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  enableScreens();
   return (
+    <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Home"
@@ -55,13 +59,13 @@ export default function App() {
             name="Item"
             options={{ headerShown: false }}
           />
-           <Stack.Screen
+          <Stack.Screen
             component={Order}
             name="Order"
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
       </NavigationContainer>
-
+    </SafeAreaProvider>
   );
 }
